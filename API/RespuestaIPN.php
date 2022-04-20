34ede1f3ad52a43a7f7292e9ba08e3d25328d930
Junior Acosta <jacosta.atwork@gmail.com>
2022-04-20 12:52:29 -0500

    make it better

--- API/RespuestaIPN.php
+++ API/RespuestaIPN.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Respuesta de la IPN</title>
 </head>
-<body>
+<body> 
 
     <title>Respuesta de la IPN</title>
 
