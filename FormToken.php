5bdf29480edc51ba92c3116ffa05e62a3a80db56
Ejemplo <jacosta.atwork@gmail.com>
2022-04-29 01:56:56 -0500

    revision

--- FormToken.php
+++ FormToken.php
@@ -1,5 +1,10 @@
 <?php
-
+    //$precio = $_POST['precio'];
+    //$moneda = $_POST['moneda'];
+    
+    //echo $precio;
+    //echo $moneda;
+    
     //datos a enviar
     $data = array(
         "amount" => $_POST['precio'], 
@@ -9,13 +14,25 @@
         "email" => "sample@example.com"
         )
     );
-   
+    
+    
+    /*
+    $url ='https://server-node-izipay.herokuapp.com/payment/init';
+      $json = file_get_contents($url);
+      $result = json_decode($json,true);
+      
+        $formtoken = trim($result);
+        var_dump($result);
+        echo $formtoken;
+    */
+    
+    
     $data_string = json_encode($data);                                                                                   
     $ch = curl_init('https://server-node-izipay.herokuapp.com/payment/init');
     //curl_setopt($ch, CURLOPT_USERPWD, "user:pwd");                                                                       
-    //curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
     curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, false);
     curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);                                                                    
     curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
         'Content-Type: application/json',
@@ -23,7 +40,8 @@
     );                                                                                                                   
     $result = curl_exec($ch);
     $formtoken = trim($result);
-    //var_dump($result);
-    //echo $formtoken;
+    var_dump($result);
+    echo $formtoken;
+    
     
 ?>
